572c95be6e49f3af5d9505ac151a2c614c4ce538
Evgen <eanikulin@yandex.ru>
2021-10-12 21:10:58 +0500

    //add generation data from DB

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import os
-import json
+# import json
+from products.models import ProductCategory, Product
 
 module_dir = os.path.dirname(__file__)
 
@@ -13,7 +14,11 @@ def index(request):
 
 
 def products(request):
-    context = {'title': 'GeekShop - Каталог'}
-    file_path = os.path.join(module_dir, 'fixtures/products.json')
-    context['products'] = json.load(open(file_path, encoding='utf-8'))
+    context = {'title': 'GeekShop - Каталог',
+               'products': Product.objects.all(),
+               'categories': ProductCategory.objects.all()
+    }
+
+    # file_path = os.path.join(module_dir, 'fixtures/products.json')
+    # context['products'] = json.load(open(file_path, encoding='utf-8'))
     return render(request, 'products/products.html', context)
