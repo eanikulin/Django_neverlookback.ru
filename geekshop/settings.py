fe3d1c6c21061ee5ab641959d8cf833dbc5a1ea3
Evgeny <oren.rulit@gmail.com>
2021-11-23 19:01:14 +0500

    //public project

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -91,10 +91,29 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'geekshop',
+        'PASSWORD': env('PASSWORD_DB'),
+        'HOST': 'minimonetaz1.ddns.net',
+        'PORT': '5432'
     }
 }
 
