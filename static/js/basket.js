a3cadff1cc898902eb9f34eddf350044e512ffc5
eanikulin <ea.nikulin@yandex.ru>
2021-12-01 18:53:32 +0500

    //add ajax for add product

--- static/js/basket.js
+++ static/js/basket.js
@@ -1,8 +1,6 @@
 window.onload = function () {
     $('.basket_list').on('click', 'input[type="number"]', function () {
         var t_href = event.target;
-        console.log(t_href.name); // basket.id
-        console.log(t_href.value); // basket.quantity
 
         $.ajax({
             url: '/baskets/edit/' + t_href.name + '/' + t_href.value + '/',
@@ -12,17 +10,14 @@ window.onload = function () {
         })
     });
 
-    // $('.product_list').on('click', 'a[type="add_item"]', function () {
-    //     var t_href = event.target;
-    //     console.log(t_href.href);
-    //     console.log('ДО ЗАПРОСА');
-    //
-    //     $.ajax({
-    //         url: t_href.href,
-    //         success: function (data) {
-    //             $('.product_list').html(data.result);
-    //             console.log('после ЗАПРОСА');
-    //         },
-    //     })
-    //     });
+    $('.product_list').on('click', 'div[type="add_item"]', function () {
+        var t_href = event.target;
+
+        $.ajax({
+            url: t_href.dataset.add_item,
+            success: function (data) {
+                $('.product_list').html(data.result);
+            },
+        })
+    });
 }
