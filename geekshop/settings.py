7474db17e8893a031715178338b0626abdf8b4a5
eanikulin <ea.nikulin@yandex.ru>
2021-12-03 19:16:34 +0500

    //add tests

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env('DEBUG')
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
