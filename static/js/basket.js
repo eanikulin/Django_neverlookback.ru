a0d678a9e9e09ec74e0011cb681d7efcc67dd6d7
eanikulin <ea.nikulin@yandex.ru>
2021-12-08 18:22:28 +0500

    //use axios

--- static/js/basket.js
+++ static/js/basket.js
@@ -1,23 +1,16 @@
 window.onload = function () {
-    $('.basket_list').on('click', 'input[type="number"]', function () {
-        var t_href = event.target;
-
-        $.ajax({
-            url: '/baskets/edit/' + t_href.name + '/' + t_href.value + '/',
-            success: function (data) {
-                $('.basket_list').html(data.result);
-            },
-        })
-    });
-
     $('.product_list').on('click', 'div[type="add_item"]', function () {
         var t_href = event.target;
-
-        $.ajax({
+        axios({
+            method: 'get',
             url: t_href.dataset.add_item,
-            success: function (data) {
-                $('.product_list').html(data.result);
-            },
         })
+        .then(function (response) {
+            console.log(response);
+            console.log('Товар добавлен');
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     });
 }
---
+++ static/js/basket_bak.js
@@ -0,0 +1,23 @@
+// window.onload = function () {
+//     $('.basket_list').on('click', 'input[type="number"]', function () {
+//         var t_href = event.target;
+//
+//         $.ajax({
+//             url: '/baskets/edit/' + t_href.name + '/' + t_href.value + '/',
+//             success: function (data) {
+//                 $('.basket_list').html(data.result);
+//             },
+//         })
+//     });
+//
+//     $('.product_list').on('click', 'div[type="add_item"]', function () {
+//         var t_href = event.target;
+//
+//         $.ajax({
+//             url: t_href.dataset.add_item,
+//             success: function (data) {
+//                 $('.product_list').html(data.result);
+//             },
+//         })
+//     });
+// }
